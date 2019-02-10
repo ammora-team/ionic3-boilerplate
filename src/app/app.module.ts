3dff4263f1a58f80e23130d3e3c2b1f1bf6f2259
ramonornela <ramon.ornela@gmail.com>
2019-02-10 18:50:12 -0200

    feat: add lazy loading

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-import { HomePage } from '../pages/home/home';
 import { MyAppComponent } from './app.component';
 
 @NgModule({
-  declarations: [MyAppComponent, HomePage],
-  imports: [BrowserModule, IonicModule.forRoot(MyAppComponent)],
+  declarations: [MyAppComponent],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyAppComponent, { preloadModules: true })
+  ],
   bootstrap: [IonicApp],
-  entryComponents: [MyAppComponent, HomePage],
+  entryComponents: [MyAppComponent],
   providers: [
     StatusBar,
     SplashScreen,
